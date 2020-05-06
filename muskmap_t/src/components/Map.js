0c465c5b8b3a7bcb7262e14da8ed240a08b1ed3f
Flavius Valerianus <atteleitus@gmail.com>
2020-05-06 16:11:20 +0900

    WTFFF

--- muskmap_t/src/components/Map.js
+++ muskmap_t/src/components/Map.js
@@ -8,20 +8,10 @@ import { bindActionCreators } from 'redux'
 import { fetchMuskmapdata } from '../action/muskmapaction'
 import 'leaflet/dist/leaflet.css'
 import L from 'leaflet'
-// import axios from 'axios'
 
-// 參考資料 : https://leafletjs.com/examples/quick-start/ & https://juejin.im/post/5cc192976fb9a032092e8e0a
 let mymap
 class Map extends React.Component {
   componentDidMount() {
-    // axios
-    //   .get(
-    //     'https://raw.githubusercontent.com/kiang/pharmacies/master/json/points.json'
-    //   )
-    //   .then(function (response) {
-    //     // Success
-    //     const muskmap = response.data
-    //   })
     this.props.fetchMuskmapdata()
     console.log(this.props)
   }
@@ -33,63 +23,6 @@ class Map extends React.Component {
       L.tileLayer(OSMUrl).addTo(mymap)
       const marksinfo = this.props.muskmapdata
       console.log(marksinfo)
-      // if (mymap !== undefined) {
-      //   mymap.remove()
-      // }
-      // const currentmark = this.props.match.params
-      // const marksinfo = this.props.mapmarksGCS.DivelocationInfo
-      // console.log(marksinfo)
-      // console.log(currentmark)
-      // const locatecurrentmark = marksinfo.filter(
-      //   (marksinfo) => marksinfo.LocationID === currentmark.LocationID
-      // )
-      // console.log(locatecurrentmark)
-      // mymap = L.map('mapid').setView(
-      //   [25.033158, 121.564532],
-      //   15
-      // )
-      // locatecurrentmark.forEach(function (locatecurrentmark) {
-      //   mymap = L.map('mapid').setView(
-      //     [25.033158, 121.564532],
-      //     15
-      //   )
-      // })
-      //locatecurrentmark.Latitude, locatecurrentmark.Longitude
-      // const OSMUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-
-      // L.tileLayer(OSMUrl).addTo(mymap)
-
-      // 使用 leaflet-color-markers ( https://github.com/pointhi/leaflet-color-markers ) 當作 marker
-      // const blueIcon = new L.Icon({
-      //   iconUrl:
-      //     'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png',
-      //   shadowUrl:
-      //     'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-      //   iconSize: [25, 41],
-      //   iconAnchor: [12, 41],
-      //   popupAnchor: [1, -34],
-      //   shadowSize: [41, 41],
-      // })
-
-      // const marksinfoarr = Object.keys(marksinfo).map(function(_) { return marksinfo[_]; })
-      // const mapmarks = marksinfoarr.flat()
-      //WTF type of ?????
-      // console.log(marksinfo)
-      // console.log(typeof marksinfo)
-
-      // console.log(currentmark)
-      // marksinfo.forEach(function (marksinfo) {
-      //   L.marker([marksinfo.Latitude, marksinfo.Longitude], { icon: blueIcon })
-      //     .addTo(mymap)
-      //     .bindPopup(marksinfo.LocationName)
-      // })
-      // 打開現在地點
-      // locatecurrentmark.forEach(function (marksinfo) {
-      //   L.marker([marksinfo.Latitude, marksinfo.Longitude], { icon: blueIcon })
-      //     .addTo(mymap)
-      //     .bindPopup(marksinfo.LocationName)
-      //     .openPopup()
-      // })
     }
   }
   render() {
